82844ea0434526bd5af8d5ac9873921420769d13
Saodat <saodat.riskiyeva@gmail.com>
2025-06-11 14:19:05 -0400

    dispatching actions

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -10,9 +10,13 @@ function AccountOperations() {
   const [currency, setCurrency] = useState("USD");
 
   const dispatch = useDispatch();
-  const account = useSelector((state) => state.account);
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((store) => store.account);
 
-  console.log(account);
+  console.log(balance);
 
   function handleDeposit() {
     if (!depositAmount) return;
@@ -91,10 +95,16 @@ function AccountOperations() {
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        {currentLoan > 0 ? (
+          <div>
+            <span>
+              Pay back ${currentLoan} ({currentLoanPurpose})
+            </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
