9d9ab60f1b9cc1848e366205e9dd8c9972ec5111
Saodat <saodat.riskiyeva@gmail.com>
2025-06-11 13:55:18 -0400

    using useSelector hook for getting state data

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
 
 function Customer() {
-  const customer = useSelector((store) => store.customer.fullName);
+  const customer = useSelector((store) => store.customer);
   console.log(customer);
-  return <h2>👋 Welcome, %NAME%</h2>;
+  return <h2>👋 Welcome, {customer.fullName}</h2>;
 }
 
 export default Customer;
