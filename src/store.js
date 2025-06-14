1a1ce206f04f1d287225dac9f0583d9860f378f1
Saodat <saodat.riskiyeva@gmail.com>
2025-06-13 20:42:47 -0400

    creating the Store with RTK

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
